c0e0ecb5bf212e68178cfb67e9b96169825a2796
Daniel Cousineau <dcousineau@gmail.com>
2024-10-26 12:17:17 -0400

    Upgrade to node@18, projen@0.88.6, aws-cdk-lib min
    - aws-cdk min version bumped to 2.93.0 preemptively
      - v2.93.0 is first time that NODEJS_LATEST runtime is introduced
      - See: https://github.com/aws/aws-cdk/commit/2aa4733e7177df58eacb9d4344fa1a68505aff28
    - Updates related projen dependencies, also updates TypeScript
    - Removes now-unnecessary build configuration overrides
    - Adds .nvmrc file for local dev

--- .projenrc.js
+++ .projenrc.js
@@ -5,17 +5,17 @@ const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Matthew Bonig',
   authorAddress: 'matthew.bonig@gmail.com',
   description: 'A construct for working with RDS SQL servers',
-  cdkVersion: '2.50.0',
+  cdkVersion: '2.93.0',
   defaultReleaseBranch: 'main',
   jsiiFqn: 'matthewbonig.rdsTools',
   name: '@matthewbonig/rds-tools',
   homepage: 'https://github.com/mbonig/rds-tools',
   repositoryUrl: 'https://github.com/mbonig/rds-tools.git',
-  jsiiVersion: '^5.1.1',
-  devDeps: [
-    'eslint',
-    'esbuild',
-  ],
+  jsiiVersion: '~5.5.0',
+  // devDeps: [
+  //   'eslint',
+  //   'esbuild',
+  // ],
   license: 'MIT',
   depsUpgrade: false,
   npmAccess: 'public',
@@ -38,11 +38,12 @@ project.addFields({
   },
 });
 
-project.gitignore.exclude('cdk.context.json', '.cdk.staging/', '.idea/', 'cdk.out/', 'npm-cache/');
-project.npmignore.exclude('cdk.context.json', '.cdk.staging/', '.idea/', 'cdk.out/');
-project.npmignore.include('./lib/handlers/*');
+// project.gitignore.exclude('cdk.context.json', '.cdk.staging/', '.idea/', 'cdk.out/', 'npm-cache/');
+// project.npmignore.exclude('cdk.context.json', '.cdk.staging/', '.idea/', 'cdk.out/');
 
-project.compileTask.exec('jsii --silence-warnings=reserved-word --no-fix-peer-dependencies && jsii-docgen && cp src/handlers/*.ts lib/handlers/ && cp src/handlers/cfn-response.js lib/handlers/ && cp -R src/layer lib/layer && cp src/handlers/package* lib/handlers/');
+// project.npmignore.include('./lib/handlers/*');
+
+// project.compileTask.exec('cp src/handlers/*.ts lib/handlers/ && cp src/handlers/cfn-response.js lib/handlers/ && cp -R src/layer lib/layer && cp src/handlers/package* lib/handlers/');
 
 
 project.synth();
